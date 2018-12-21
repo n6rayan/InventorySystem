2cb52d66bde3414d14101f5ea60b6509261ad9d6
Nour Rayan <nour@broadbean.com>
2018-12-21 16:56:12 +0000

    Added test for health check route.

--- app.js
+++ app.js
@@ -12,3 +12,5 @@ app.get('/healthcheck', (req, res) => {
 app.listen(3000, () => {
     console.log("Server started on port 3000.\nGo to http://127.0.0.1:3000 to see the health check.");
 });
+
+module.exports = app;

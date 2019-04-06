05f4831004afd7bee8955bb3784252a1deb29713
Nour Rayan <nour@broadbean.com>
2019-04-06 09:09:22 +0100

    Added user model.

---
+++ src/database/models/user.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+
+interface User extends mongoose.Document {
+    _id: string,
+    username: string,
+    password: string,
+    email: string,
+    fullName: string
+}
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    fullName: { type: String, required: true }
+});
+
+const User = mongoose.model<User>('Item', UserSchema);
+export { User };

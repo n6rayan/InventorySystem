47c9aae31425fc38bc7d92d4f5a731c766b00414
Nour Rayan <nour@broadbean.com>
2019-04-14 16:00:18 +0100

    IS-18: Complete test coverage of user db functionality

--- test/users.ts
+++ test/users.ts
@@ -7,44 +7,60 @@ const db = new Database();
 let mongod: MongoMemoryServer;
 
 before((done) => {
-	mongod = new MongoMemoryServer();
+    mongod = new MongoMemoryServer();
+    mongod.getConnectionString().then(mongoUri => {
+        db._connect(mongoUri);
+    });
 
-	mongod.getConnectionString()
-	.then(mongoUri => {
-		db._connect(mongoUri);
-
-	})
-	.then(() => done());
+    done();
 });
 
 after(() => {
-	mongod.stop();
+    mongod.stop();
 });
 
 const userData = {
-	username: 'username',
-	password: 'password',
-	email: 'email@fake-email.com',
-	fullName: 'User Name'
+    username: 'username',
+    password: 'password',
+    email: 'email@fake-email.com',
+    fullName: 'User Name'
 };
 
 describe('Users Test', () => {
-	it('should create a user', (done) => {
-		db.createUser(userData).then(user => {
-			expect(user).to.be.an('object');
-			expect(user['id']).to.be.string;
-			expect(user['email']).to.equal('email@fake-email.com');
-		});
+    let userId: string;
 
-		done();
-	});
+    it('should create a user', async () => {
+        const createUser = await db.createUser(userData);
+        userId = createUser['id'];
 
-	it('should fetch a user', (done) => {
-		db.fetchUser('edfdsa')
-		.then(user => {
-			console.log(user);
-		});
+        expect(createUser).to.be.an('object');
+        expect(createUser['id']).to.be.string;
+        expect(createUser['email']).to.equal('email@fake-email.com');
+    });
 
-		done();
-	});
+    it('should fetch a user', async () => {
+        const fetchUser = await db.fetchUser(userId);
+
+        expect(fetchUser['username']).to.equal('username');
+        expect(fetchUser['password']).to.equal('password');
+        expect(fetchUser['fullName']).to.equal('User Name');
+    });
+
+    it('should update a user', async () => {
+        const updateUserData = {
+            username: 'u23rn4m3',
+            fullName: 'New User Name'
+        };
+        const updateUser = await db.updateUser(userId, updateUserData);
+
+        expect(updateUser['username']).to.equal('u23rn4m3');
+        expect(updateUser['password']).to.equal('password');
+        expect(updateUser['fullName']).to.equal('New User Name');
+    });
+
+    it('should delete a user', async () => {
+        const deleteUser = await db.deleteUser(userId);
+
+        expect(deleteUser['id']).to.equal(userId);
+    });
 });

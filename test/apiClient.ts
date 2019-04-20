180b245bc648d502f71164e11de6d1c25a349223
Nour Rayan <nour@broadbean.com>
2019-04-20 18:22:55 +0100

    IS-23: Mock API calls with Nock

--- test/apiClient.ts
+++ test/apiClient.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import * as nock from 'nock';
 import { APIClient } from '../src/apiClient';
 
 const apiClient = new APIClient();
@@ -11,12 +12,14 @@ describe('Test API Client', () => {
             data: {
                 name: "Nour Rayan",
                 job: "Developer"
-            },
-            json: true
+            }
         };
 
+        nock('https://reqres.in')
+        .post('/api/users', { name: "Nour Rayan", job: "Developer" })
+        .reply(200, { name: "Nour Rayan", id: 123, createdAt: "blah", job: "Developer" });
+
         const request = await apiClient.makeRequest(options);
-        
         expect(request['name']).to.be.string;
         expect(request).have.property('id');
         expect(request).have.property('createdAt');
@@ -26,9 +29,19 @@ describe('Test API Client', () => {
     it('should fetch and return a user', async () => {
         const options = {
             method: 'GET',
-            url: 'https://reqres.in/api/users/2',
-            json: true
+            url: 'https://reqres.in/api/users/2'
         };
+
+        nock('https://reqres.in')
+        .get('/api/users/2')
+        .reply(200, {
+            data: {
+                id: 2,
+                first_name: 'Janet',
+                last_name: 'Weaver',
+                avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg'
+            }
+        });
 
         const request = await apiClient.makeRequest(options);
         expect(request['data']).to.deep.include({

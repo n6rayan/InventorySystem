c7ab42849683cd2a616184efdbd19378900e5aa1
Nour Rayan <nour@broadbean.com>
2019-04-21 10:45:50 +0100

    IS-16: Added test coverage

--- test/app/healthcheck.ts
+++ test/app/healthcheck.ts
@@ -1,13 +1,16 @@
-import { expect, request } from 'chai';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
 import app from '../../src/app';
+
+chai.use(chaiHttp);
 
 describe('Check HealthCheck Route', () => {
 
     it('it should GET back a health check response', async () => {
-        const res = await request(app).get('/healthcheck');
+        const res = await chai.request(app).get('/healthcheck');
 
-        expect(res.status).to.equal(200);
-        expect(res.body).to.be('object');
-        expect(res.body.message).to.equal('OK!');
+        chai.expect(res.status).to.equal(200);
+        chai.expect(res.body).to.be.an('object');
+        chai.expect(res.body.message).to.equal('OK!');
     });
 });
--- test/app/item.ts
+++ test/app/item.ts
@@ -1,50 +1,53 @@
-import { expect, request } from 'chai';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
 import app from '../../src/app';
+
+chai.use(chaiHttp);
 
 describe('Check Item API Functionality', () => {
     let itemId: string;
 
     it('should create item', async () => {
-        const res = await request(app)
+        const res = await chai.request(app)
         .post('/item')
         .send({ itemName: 'Milk', description: 'A carton of Milk.', price: 0.99 });
 
         itemId = res.body.itemId;
         
-        expect(res.body).to.be.an('object');
-        expect(res.body.success).to.equal(1);
-        expect(res.body.message).to.match(/been created/);
-        expect(res.body.itemId).to.be.string;
+        chai.expect(res.body).to.be.an('object');
+        chai.expect(res.body.success).to.equal(1);
+        chai.expect(res.body.message).to.match(/been created/);
+        chai.expect(res.body.itemId).to.be.string;
     });
 
     it('should not successfully fetch item', async () => {
-        const res = await request(app).get('/item/12345');
+        const res = await chai.request(app).get('/item/12345');
 
-        expect(res.body.success).to.equal(0);
-        expect(res.body.message).to.match(/Cast to ObjectId/);
+        chai.expect(res.body.success).to.equal(0);
+        chai.expect(res.body.message).to.match(/Cast to ObjectId/);
     });
 
     it('should successfully fetch item', async () => {
-        const res = await request(app).get(`/item/${itemId}`);
+        const res = await chai.request(app).get(`/item/${itemId}`);
         
-        expect(res.body.description).to.equal('A carton of Milk.');
-        expect(res.body.itemName).to.equal('Milk');
-        expect(res.body.price).to.equal(0.99);
+        chai.expect(res.body.description).to.equal('A carton of Milk.');
+        chai.expect(res.body.itemName).to.equal('Milk');
+        chai.expect(res.body.price).to.equal(0.99);
     });
 
     it('should update item', async () => {
-        const res = await request(app)
+        const res = await chai.request(app)
         .put(`/item/${itemId}`)
         .send({ itemName: 'Cheese', description: 'A block of cheese.' });
 
-        expect(res.body.success).to.equal(1);
-        expect(res.body.message).to.match(/updated!/);
+        chai.expect(res.body.success).to.equal(1);
+        chai.expect(res.body.message).to.match(/updated!/);
     });
 
     it('should delete item', async () => {
-        const res = await request(app).delete(`/item/${itemId}`);
+        const res = await chai.request(app).delete(`/item/${itemId}`);
 
-        expect(res.body.success).to.equal(1);
-        expect(res.body.message).to.match(/deleted!/);
+        chai.expect(res.body.success).to.equal(1);
+        chai.expect(res.body.message).to.match(/deleted!/);
     });
 });

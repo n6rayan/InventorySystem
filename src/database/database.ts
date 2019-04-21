139801649aea161c7521bbd47f71f1b8b24ef073
Nour Rayan <nour@broadbean.com>
2019-04-21 10:45:23 +0100

    Minor updates to database module

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,6 +3,9 @@ import * as mongoose from 'mongoose';
 
 import { Item } from './models/item';
 import { User } from './models/user';
+import { Logger } from '../logger';
+
+const logger = new Logger();
 
 export class Database {
     constructor() {
@@ -10,10 +13,16 @@ export class Database {
         this._connect(mongooseConfig['connectionString']);
     }
 
-    _connect(connection: string) {
+    async _connect(connection: string) {
         mongoose.set('useNewUrlParser', true);
         mongoose.set('useFindAndModify', false);
-        mongoose.connect(connection);
+
+        try {
+            await mongoose.connect(connection);
+        }
+        catch (err) {
+            logger.error(err);
+        }
     }
 
     public fetchItem(id: string) {
@@ -42,7 +51,7 @@ export class Database {
 
     public deleteItem(id: string) {
         return new Promise((resolve, reject) => {
-            Item.findByIdAndDelete(id)
+            Item.findOneAndRemove(id)
             .then(item => resolve(item))
             .catch(err => reject(err));
         });
@@ -50,41 +59,33 @@ export class Database {
 
     public fetchUser(id: string) {
         return new Promise((resolve, reject) => {
-            User.findById(id).then(user => {
-                resolve(user)
-            }).catch(err => {
-                reject(err);
-            });
+            User.findById(id)
+            .then(user => resolve(user))
+            .catch(err => reject(err));
         });
     }
 
     public createUser(userInfo: object) {
         return new Promise((resolve, reject) => {
-            User.create(userInfo).then(user => {
-                resolve(user)
-            }).catch(err => {
-                reject(err);
-            });
+            User.create(userInfo)
+            .then(user => resolve(user))
+            .catch(err => reject(err));
         });
     }
 
     public updateUser(id: string, userInfo: object) {
         return new Promise((resolve, reject) => {
-            User.findOneAndUpdate({_id: id}, userInfo, {new: true}).then(user => {
-                resolve(user)
-            }).catch(err => {
-                reject(err);
-            });
+            User.findOneAndUpdate({_id: id}, userInfo, {new: true})
+            .then(user => resolve(user))
+            .catch(err => reject(err));
         });
     }
 
     public deleteUser(id: string) {
         return new Promise((resolve, reject) => {
-            User.findOneAndDelete({_id: id}).then(user => {
-                resolve(user)
-            }).catch(err => {
-                reject(err);
-            });
+            User.findOneAndRemove({_id: id})
+            .then(user => resolve(user))
+            .catch(err => reject(err));
         });
     }
 }
